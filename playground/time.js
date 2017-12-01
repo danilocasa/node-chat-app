6868dafcddf137e5c068d8b101c2f949a57be722
Danilo Casa <danilo.casa@gmail.com>
2017-12-01 17:03:15 +0100

    Format timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+const moment = require('moment');
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
